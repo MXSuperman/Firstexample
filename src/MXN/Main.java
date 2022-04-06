cc7400ec2221d15e0d22cff074510107bcd203d2
superman <squatforrest@gmail.com>
2022-04-06 10:17:38 -0500

    Первое выполненное задание

--- src/MXN/Main.java
+++ src/MXN/Main.java
@@ -3,6 +3,30 @@ package MXN;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("hello world");
+
+        // Задание 6
+        int a = 12;
+        int b = 27;
+        int c = 44;
+        int d = 15;
+        int e = 9;
+        int result = -(a * (b + (c - d * e)));
+        {
+            System.out.println(result);
+        }
+        // Задание 7
+
+        byte x = 7;
+        byte y = 5;
+        System.out.println("x = " + y + " y = " + x);
+
+        // Задание 8
+
+        int number = 750;
+        int m = number / 10;
+        int n = number / m;
+        int k = n / 2;
+        System.out.println(k);
+        }
     }
-}
+
