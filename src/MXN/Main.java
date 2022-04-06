2d578bc6b96926ca8eefecd81dd79cb15544fe1f
superman <squatforrest@gmail.com>
2022-04-06 15:15:23 -0500

    Первое выполненное задание

--- src/MXN/Main.java
+++ src/MXN/Main.java
@@ -10,7 +10,7 @@ public class Main {
         int c = 44;
         int d = 15;
         int e = 9;
-        int result = -(a * (b + (c - d * e)));
+        int result = (a * (b + (c - d * e)));
         {
             System.out.println(result);
         }
@@ -18,15 +18,16 @@ public class Main {
 
         byte x = 7;
         byte y = 5;
-        System.out.println("x = " + y + " y = " + x);
+        System.out.println("y = " + (y + x - y));
+        System.out.println("x = " + ((x + y) - x));
 
         // Задание 8
 
-        int number = 750;
+        int number = 326;
+        int number2 = 2;
         int m = number / 10;
-        int n = number / m;
-        int k = n / 2;
-        System.out.println(k);
+        int n = m / number + number2;
+        System.out.println(n);
         }
     }
 
